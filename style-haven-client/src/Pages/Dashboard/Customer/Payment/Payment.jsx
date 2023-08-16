5f7c5ed61c57cfac6685a6cd05b908366772c4dc
Sazzads <sazzadtk@gmail.com>
2023-08-17 00:10:56 +0600

    Stripe install and initial setup

--- style-haven-client/src/Pages/Dashboard/Customer/Payment/Payment.jsx
+++ style-haven-client/src/Pages/Dashboard/Customer/Payment/Payment.jsx
@@ -1,8 +1,13 @@
 import React, { useState } from 'react';
 import { FaCcAmex, FaCcStripe, FaMobileAlt } from 'react-icons/fa';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import Stripe from './Stripe/Stripe';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
 
 const Payment = () => {
+    const stripePromise = loadStripe(import.meta.env.VITE_PAYMENT_GATEWAY_PK);
+    
     return (
         <>
             <h2 className="text-3xl text-center my-8">Patment</h2>
@@ -10,11 +15,12 @@ const Payment = () => {
                 <TabList className='flex cursor-pointer items-center justify-center gap-10 mb-5'>
                     <Tab className="btn"><FaCcStripe></FaCcStripe> Card Payment</Tab>
                     <Tab className="btn"><FaMobileAlt></FaMobileAlt> Mobile Banking</Tab>
-                   
+
                 </TabList>
 
                 <TabPanel>
-                    <h2>Any content 1</h2>
+                    <Elements stripe={stripePromise}><Stripe ></Stripe></Elements>
+
                 </TabPanel>
                 <TabPanel>
                     <h2>Any content 2</h2>
